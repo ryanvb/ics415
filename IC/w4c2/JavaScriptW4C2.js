f33203fb0d28341dd561afbd0814f264017e51f7
Ryan <ryanvb@hawaii.edu>
2015-09-17 11:41:07 -1000

    w4c2 javascript(not finished)
    here is what i had so far in class today

---
+++ IC/w4c2/JavaScriptW4C2.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Vairoa on 9/15/2015.
+ */
+function myFunction() {
+
+    var inPut = document.getElementById("myForm").elements[0].value;
+    var inPut2 = document.getElementById("myForm").elements[1].value;
+    var inPut3 = document.getElementById("myForm").elements[2].value;
+    var inPut4 = document.getElementById("myForm").elements[3].value;
+    var inPut5 = document.getElementById("myForm").elements[4].value;
+}
+
+function testResults() {
+    var TestVar = document.getElementById("myForm").elements[0].value;
+    alert ("You typed: " + TestVar);
+}
