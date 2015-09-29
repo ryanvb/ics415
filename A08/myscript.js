4ad3dec40a2a8871fb0cd2f9ef347f60e0b0a930
Ryan <ryanvb@hawaii.edu>
2015-09-29 01:02:10 -1000

    ICS415 Assignment 8
    Here is my submission for assignment 8

---
+++ A08/myscript.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Vairoa on 9/21/2015.
+ */
+$(document).ready(function(){
+    $("#b1").click(function(){
+        $("#ans1").toggle();
+    });
+
+    $("#b2").click(function(){
+        $("#ans2").toggle();
+    });
+
+    $("#b3").click(function(){
+        $("#ans3").toggle();
+    });
+    $("#b4").click(function(){
+        $("#ans4").toggle();
+    });
+
+    $("#b5").click(function(){
+        $("#ans5").toggle();
+    });
+});
+
+
+
